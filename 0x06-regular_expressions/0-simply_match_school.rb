ba8a1931164652e829fbd4c38b3843a9d6ee9f0e
Opeoluwadigba <opeoluwadigba.idowu@gmail.com>
2022-01-19 18:10:01 +0000

    School matched.

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,8 +1,3 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/School[a-z]/).join
-root@e4eefdd5f09d$ ./0-simply_match_holberton.rb "School" | cat -e
-School$
-root@e4eefdd5f09d$ ./example.0-simply_match_school.rb "Sthool" | cat -e
-School$
-root@e4eefdd5f09d$ ./example.0-simply_match_school.rb "Neha loves School, that is why she is a School mentor :)" | cat -e
-School$
+#Should match 'School'
+puts ARGV[0].scan(/School/).join

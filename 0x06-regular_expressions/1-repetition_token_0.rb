dee8dc8effb78727d2f7ea74c5bbdc593e896f43
Opeoluwadigba <opeoluwadigba.idowu@gmail.com>
2022-01-19 18:15:18 +0000

    Repeating 'n'

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,8 +1,2 @@
 #!/usr/bin/env Ruby
-puts ARGV[0].scan(/hbtn[a-z]/).join
-root@e4eefdd5f09d$
-root@e4eefdd5fo9d$ ./1-repetition_token_0.rb hbttttttn | cat -e 
-hbtn$
-root@e4eefdd5f09d$ ./1-repetition_token_0.rb hbtttn | cat -e
-hbtn$
-sylvain@ubuntu$ ./1-repetition_token_0.rb hbttn | cat -e
+puts ARGV[0].scan(/hbt{2,5}n/).join

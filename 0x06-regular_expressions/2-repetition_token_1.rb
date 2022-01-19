721036c123f0d6bd155adf8a12082f7c64456f80
Opeoluwadigba <opeoluwadigba.idowu@gmail.com>
2022-01-19 18:33:30 +0000

    Second token.

--- 0x06-regular_expressions/2-repetition_token_1.rb
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -1,11 +1,3 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/hbtn[a-z]b*)join
-root@e4eefdd5f09d$
-root@e4eefdd5f09d$ ./2-repetition_token_1.rb htn | cat -e 
-hbtn$
-root@e4eefdd5f09d$ ./2-repetition_token_1.rb hbtn | cat -e
-hbtn$
-root@e4eefdd5f09d$ ./2-repetition_token_1.rb hbbtn | cat -e
-hbtn$
-root@e4eefdd5f09d$ ./2-repetition_token_1.rb hbbbtn | cat -e
-$
+#Match hbbtn and hbbbtn
+puts ARGV[0].scan(/hb?tn).join
